fda7e54d52fc98708c02e9cf1da0a5236e151703
Andrey <andyvesel@ya.ru>
2015-05-26 14:45:10 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
-  get 'vacancies/index'
 
+  resources :employees do
+    resources :skills
+  end
+
+  resources :vacancies do
+    resources :skills
+  end
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'employees#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
