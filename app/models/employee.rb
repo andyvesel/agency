f2ac61d68faa387af662ecc59c4f0a0c6ae021e2
Andrey <andyvesel@ya.ru>
2015-05-27 12:16:24 +0300

    Add simple validation to Employee

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,7 @@
 class Employee < ActiveRecord::Base
+  validates :name, presence: true
+  validates :contact, presence: true
+  validates :salary, presence: true
+
   has_many :skills, as: :skillable
 end
