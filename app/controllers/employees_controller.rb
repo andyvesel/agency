c6be5473262ba4b4bd5377190cc0ad38265c4de2
Andrey <andyvesel@ya.ru>
2015-05-27 12:09:12 +0300

    Fix employees controller

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,20 @@
 class EmployeesController < ApplicationController
+  def index
+    @employees = Employee.all
+  end
+
+  def new
+  end
+
+  def create
+    @employee = Employee.new(employee_params)
+    @employee.save
+
+    redirect_to employees_path
+  end
+
+  private
+  def employee_params
+    params.require(:employee).permit(:name, :contact, :search, :salary)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'employees/index'
+
   resources :employees do
     resources :skills
   end
