cd366db8ac643ddde8d76616aaae57bed2129d88
Andrey <andyvesel@ya.ru>
2015-05-27 13:36:08 +0300

    Update create action for employee

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -8,12 +8,17 @@ class EmployeesController < ApplicationController
 
   def create
     @employee = Employee.new(employee_params)
-    @employee.save
+    if @employee.valid?
+      @employee.save
 
-    redirect_to employees_path
+      redirect_to employees_path
+    else
+      render action: 'new'
+    end
   end
 
   private
+  
   def employee_params
     params.require(:employee).permit(:name, :contact, :search, :salary)
   end
