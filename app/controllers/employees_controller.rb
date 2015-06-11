5cdcebbf4e8295c370afcfb854a7cda4a144dcc6
Andrey <andyvesel@ya.ru>
2015-06-11 16:58:21 +0300

    Add show, update & destroy actions

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -3,6 +3,10 @@ class EmployeesController < ApplicationController
     @employees = Employee.all
   end
 
+  def show
+    @employee = Employee.find(params[:id])
+  end
+
   def new
   end
 
@@ -17,8 +21,29 @@ class EmployeesController < ApplicationController
     end
   end
 
+  def edit
+    @employee = Employee.find(params[:id])
+  end
+
+  def update
+    @employee = Employee.find(params[:id])
+
+    if @employee.update(employee_params)
+      redirect_to @employee
+    else
+      render action: 'new'
+    end
+  end
+
+  def destroy
+    @employee = Employee.find(params[:id])
+    @employee.destroy
+
+    redirect_to employees_path
+  end
+
   private
-  
+
   def employee_params
     params.require(:employee).permit(:name, :contact, :search, :salary)
   end
