91bea2e6cbfdebd208c73e23948988f93b83c4e7
Andrey <andyvesel@ya.ru>
2015-06-15 15:26:07 +0300

    Add 'new' & 'create' actions for vacancies controller

--- app/controllers/vacancies_controller.rb
+++ app/controllers/vacancies_controller.rb
@@ -2,4 +2,27 @@ class VacanciesController < ApplicationController
   def index
     @vacancies = Vacancy.all
   end
+
+  def new    
+  end
+
+  def create
+    @vacancy = Vacancy.new(vacancy_params)
+
+    if @vacancy.valid?
+      @vacancy.save
+
+      redirect_to vacancies_path
+    else
+      render action: 'new'
+    end
+
+  end
+
+
+  private
+
+  def vacancy_params
+    params.require(:vacancy).permit(:title, :added, :expiration, :salary, :contacts)
+  end
 end
