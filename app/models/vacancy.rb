ee030c8fbd22b08a78be936a4c464d07a0c01ba0
Andrey <andyvesel@ya.ru>
2015-06-15 15:19:15 +0300

    Add simple validation to Vacancy model

--- app/models/vacancy.rb
+++ app/models/vacancy.rb
@@ -1,3 +1,9 @@
 class Vacancy < ActiveRecord::Base
+  validates :title, presence: true
+  validates :added, presence: true 
+  validates :expiration, presence: true
+  validates :salary, presence: true
+  validates :contacts, presence: true
+  
   has_many :skills, as: :skillable
 end
