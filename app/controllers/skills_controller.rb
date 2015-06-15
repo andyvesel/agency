c4fa2c3257806221089c496b57394f9563d30706
Andrey <andyvesel@ya.ru>
2015-06-15 14:09:44 +0300

    Add 'create' action to skills_controller

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,2 +1,14 @@
 class SkillsController < ApplicationController
+  def create
+    @employee = Employee.find(params[:employee_id])
+    @employee.skills.create(skill_params)
+
+    redirect_to employee_path(@employee)
+  end
+
+  private
+
+  def skill_params
+    params.require(:skill).permit(:title)
+  end
 end
